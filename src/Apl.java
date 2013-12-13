aa0f2c8808c2c7b82e7b6594c94ce744f548a78f
EIN1e-Ralph <330915@student.saxion.nl>
2013-12-13 21:08:58 +0100

    testen added

--- src/Apl.java
+++ src/Apl.java
@@ -3,11 +3,23 @@ public class Apl {
 
 	/**
 	 * @param args
+	 * instantiert 4 testen met verschillende input om de N & H te testen en aan te tonen 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		Test test = new Test();
+		Test test2 = new Test();
+		Test test3 = new Test();
+		Test test4 = new Test();
+		//test1
 		test.runTest(10, 1000, 10);
+		//test2
+		test2.runTest(10, 500, 10);
+		//test3
+		test3.runTest(5, 1000, 5);
+		//test4
+		test4.runTest(5, 500, 5);
 	}
 
 }
