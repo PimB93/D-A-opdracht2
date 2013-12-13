bfdb443e6ce005fc90a7e49ef7ffef4bbd3e5778
PimB93 <p.beuwer@gmail.com>
2013-12-13 17:06:27 +0100

    Hoi, opzich af, zie whatsapp voor details

--- src/Apl.java
+++ src/Apl.java
@@ -1,76 +1,13 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.Scanner;
 
-
-public class Apl
-{
-
+public class Apl {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
 		
-        Opdracht2 opdr2;
-        File invoer = new File("input.txt");
-        PrintWriter writer =null;
-        Scanner scanner = null;
-
-        try 
-        {
-             writer = new PrintWriter(invoer);
-            
-        	 for(int i=0; i< 10; i++)
-			 {
-				
-				 writer.print(Opdracht2.randInt(70));
-				 writer.print(", ");
-			 }	
-        	 
-        } 
-        catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        finally
-        
-        {
-        	writer.close();
-        }
-        
-      try
-      {
-    	 scanner = new Scanner(invoer);
-     	 scanner.useDelimiter(", ");
-     	 String gegevens = "";
-     	 boolean eerste=false;
-     	 while(scanner.hasNext())
-     	 {
-     		 if(!eerste)
-     		 {
-     		 eerste = true;
-     		 
-     		 gegevens +=  scanner.next();
-     		 }
-     		 else
-     		 {
-     			 gegevens += ", " + scanner.next();
-     		 }
-     	 }
-     	 opdr2 = new Opdracht2(gegevens);
-      }
-      catch(FileNotFoundException e)
-      {
-    	  e.printStackTrace();
-      }
-      finally
-      {
-    	  scanner.close();
-      }
+		Test test = new Test();
+		test.runTest(10, 1000, 10);
 	}
 
 }
--- src/RSHeap.java
+++ src/RSHeap.java
@@ -1,69 +1,173 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 
 public class RSHeap {
-	private int[] unsortedArray;
-	private int[] theHeap;
-	private int size;
+	private int[] input;
+	private Heap theHeap;		
+	private int inputIndex = 0;
+	private int heapSize = 5;
+	private FileGen filegen;	
+	private BufferedWriter bw;	
+	private ArrayList<String> runFileNames;
 	
-	public RSHeap(int[] unsortedArray)
+	public RSHeap(int N)
 	{
-		this.unsortedArray = unsortedArray;
-		// -1 zodat hij op locatie 0 kan beginnen in de insert methode
-		size = -1;
-		
-		theHeap = new int[unsortedArray.length ];
-	
-		sortHeap();
-		// een uitkomst: [0, 9, 13, 43, 17, 16, 62, 48, 69, 44, 33].. Klopt volgens mij
+		heapSize = N;
+		input = new int[N];
+		this.theHeap = new Heap(heapSize);		
+		filegen = new FileGen();
+		runFileNames = new ArrayList<String>();		
 		
 		
 		
 	}
 	
-	public void sortHeap()
+	public void createInputFile(int N)
 	{
-		for(int i = 0; i < unsortedArray.length; i++)
+		input = filegen.generate(N, "numbers.txt");
+	}
+	
+	public void printArray(int [] arrayToPrint)
+	{
+		System.out.println(Arrays.toString(arrayToPrint));
+	}
+	
+	public void createRuns()
+	{
+		int runNr = 0;
+		
+	
+		//while IN is not empty
+		while(!inputEmpty())
 		{
-			insert(unsortedArray[i]);
+			
+			theHeap = buildHeap();			
+			runNr++;
+			String fileName = "run " + runNr +".txt";
+			bw = initWriter(fileName);
+			
+			//Zolang er nog elementen gelezen kunnen worden en er bestaan nog elementen 
+			while(theHeap.getSize() > -1 && !inputEmpty())
+			{
+				int next = getNext();
+				
+				//Root gaat naar de output
+				writeToFile(bw, theHeap.getMin());
+
+				
+				//Als next groter is dan de root, sla het dan op als root
+				if(next >= theHeap.getMin())
+				{
+					theHeap.setMin(next);
+				}
+				else
+				{
+					//Element is dus kleiner dan de root, daarom wordt laatste element als root gezet
+					theHeap.setElement(0, theHeap.getElement(theHeap.getSize()));
+					//Op de oude locatie wordt het volgende element geplaatst
+					theHeap.setElement(theHeap.getSize(), next);
+					//Laatste is deadspace
+					theHeap.moveToDeadSpace();
+				}
+				//Heap wordt weer als minheap ingesteld
+				theHeap.bubbledown(0);
+				
+			}
+			
 		}
-		printHeap();
-	}
-	
-	public void insert(int element)
-	{
-		size++;
-		theHeap[size] = element;
-		int position = size;
-		printHeap();
-		//Zolang de invoer kleiner is moet er geswapped tot de invoer op de juiste plek staat. De parent moet immers kleiner zijn dan haar kinderen
-		while(theHeap[position] < theHeap[parent(position)])
-		{
-			//Er wordt geswapped in de heap
-			swap(position, parent(position));
-			//De position wordt de parent
-			position = parent(position);
-			printHeap();
-		}
+		
 		
 	}
 	
-	private int parent(int pos)
+			
+
+	
+	public Heap buildHeap()
 	{
-		//Want parent staat altijd op de locatie van: child / 2
-		return pos / 2;
+		Heap oldHeap = theHeap;
+		Heap newHeap = new Heap(heapSize);		
+		
+		int nrOfElements = 0;
+		//Bekijk of het eerste aangemaakte heap is
+		if(oldHeap != null)
+		{
+			//als het een deadspace heeft doe dan dit
+			if(oldHeap.getSize() != oldHeap.getArray().length)
+			{
+				for(int i = oldHeap.getSize() + 1; i < oldHeap.getArray().length - 1; i++)
+				{
+					newHeap.insert(oldHeap.getArray()[i]);
+					nrOfElements++;
+				}
+			}
+		}
+		//Heap is al vaker gebruikt
+		for(int i = nrOfElements; i < heapSize - 1; i++)
+		{
+			newHeap.insert(getNext());
+			nrOfElements++;
+			//printArray(newHeap);
+		}
+		return newHeap;
 	}
 	
-	private void swap(int i,int y)
+	
+		
+	
+	private int getNext()
 	{
-		int temp = theHeap[i];
-		theHeap[i] = theHeap[y];
-		theHeap[y] = temp;
+		int temp = input[inputIndex];
+		inputIndex++;
+		return temp;
 	}
 	
-	private void printHeap()
+
+	public ArrayList<String> getRunFileNames()
 	{
-		System.out.println(Arrays.toString(theHeap));
+		return runFileNames;
+	}
+	
+	public BufferedWriter initWriter(String fileName)
+	{
+		try {
+
+			bw = new BufferedWriter(new FileWriter(new File(fileName)));
+			runFileNames.add(fileName);
+			System.out.println("new run started, now writing to: " + fileName);
+			return bw;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void writeToFile(BufferedWriter bw, int element)
+	{
+		try {
+			bw.write(Integer.toString(element));
+			bw.newLine();
+			bw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	private boolean inputEmpty()
+	{
+		if(input.length  == inputIndex + 1)
+		{
+			return true;
+		}
+		else
+		{
+		return false;
+		}
 	}
 	
 }
